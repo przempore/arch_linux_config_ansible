5fcd1d761694741ac0567145a86126f10897e40e
Przemek <przempore@gmail.com>
2021-09-05 20:53:35 +0200

    update `pull.sh`

--- pull.sh
+++ pull.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 SERVER_NAME=proxy_server
-REPO=git@github.com:przempore/dotfiles.git
-KEY_FILE=/home/$USER/.ssh/id_rsa
+REPO=https://github.com/przempore/arch_linux_config_ansible.git
 INVENTORY_FILE=inventory.yml
 BRANCH_NAME=main
 
-ansible-pull --url $REPO --key-file $KEY_FILE --inventory $INVENTORY_FILE packages.yml --checkout $BRANCH_NAME --force --ask-become-pass
+ansible-pull --url $REPO  --inventory $INVENTORY_FILE packages.yml main_config.yml --checkout $BRANCH_NAME --force --ask-become-pass
 
