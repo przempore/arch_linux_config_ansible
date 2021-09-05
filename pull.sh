38de5dfc8620fd4da5ff0864e850845c2ac3a65b
Przemek <przempore@gmail.com>
2021-09-05 21:16:19 +0200

    use `exec_username`

--- pull.sh
+++ pull.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-SERVER_NAME=proxy_server
+USER=<your username here>
 REPO=https://github.com/przempore/arch_linux_config_ansible.git
-INVENTORY_FILE=127.0.0.1
+INVENTORY_FILE=127.0.0.1,
 BRANCH_NAME=main
 
-ansible-pull --url $REPO -connection=local --inventory $INVENTORY_FILE packages.yml main_config.yml --checkout $BRANCH_NAME --force --ask-become-pass
+ansible-pull --url $REPO -connection=local --inventory $INVENTORY_FILE packages.yml main_config.yml --checkout $BRANCH_NAME --force --ask-become-pass --extra-vars "exec_username=$USER"
 
