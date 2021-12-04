1eff14c33d333309ace27dfd3b23820b84915cc9
Przemek <przempore@gmail.com>
2021-12-04 23:52:41 +0100

    install AUR packages using _paru_

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Arch Linux configuration using ansible.
+
+Install [ansible-aur](https://github.com/kewlfft/ansible-aur) with `ansible-galaxy`:
+```
+ansible-galaxy collection install kewlfft.aur
+```
+
--- pull.sh
+++ pull.sh
@@ -5,5 +5,5 @@ REPO=https://github.com/przempore/arch_linux_config_ansible.git
 INVENTORY_FILE=127.0.0.1,
 BRANCH_NAME=main
 
-ansible-pull --url $REPO -connection=local --inventory $INVENTORY_FILE packages.yml main_config.yml --checkout $BRANCH_NAME --force --ask-become-pass --extra-vars "exec_username=$USER"
+ansible-pull --url $REPO -connection=local --inventory $INVENTORY_FILE packages.yml main_config.yml --checkout $BRANCH_NAME --force --ask-become-pass --extra-vars "ansible_user=$USER"
 
